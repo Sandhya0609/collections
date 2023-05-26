7c5e34a9851a4fc173c07e8e8dae414dd6600c57
Sandhya0609 <sandhya0609a@gmail.com>
2023-05-26 14:32:55 -0400

    adding few

--- exercises/src/main/java/vector/Magnitude.java
+++ exercises/src/main/java/vector/Magnitude.java
@@ -7,14 +7,29 @@ package vector;
 import java.util.Vector;
 
 public class Magnitude {
-    public static void main(String[] args) {
-        Vector<Integer> myData=new Vector<>();
-        myData.add(1);
-        myData.add(2);
-        myData.add(3);
 
+    // method to calculate the sum of the squares of vector elements
+    public Double calMagnitude(Vector<Double> v) {
+        Double result = (v.get(0) * v.get(0)) + (v.get(1) * v.get(1)) + (v.get(2) * v.get(2));
+        return result;
     }
 
+    public static void main(String[] args) {
+
+        Vector<Double> myData=new Vector<>();
+
+        //vector is a=1x+2y+3z
+        myData.add(1.0);
+        myData.add(2.0);
+        myData.add(3.0);
+
+        Magnitude magnitude=new Magnitude();
+        Double result=magnitude.calMagnitude(myData);
+
+        //Calling Math.sqrt method
+        System.out.println("Magnitude of the given vector is: "+Math.sqrt(result));
+
+    }
 
     }
 
